a7301c25506ddb2b26eea520b14653336191dc26
ntvs <NickRibs@hotmail.com>
2023-04-26 18:59:47 -0400

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<div class="error">
+    <h1>Page not found.（︶︿︶）</h1>
+</div>
+
+<style>
+    .error {
+        color: lightgray;
+        text-align: center;
+    }
+
+    .error > * {
+        padding: 1em 0;
+    }
+</style>
