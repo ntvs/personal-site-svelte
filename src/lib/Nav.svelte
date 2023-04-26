5280db7af40a04ebed7d94c3223dc82cd0c54d00
ntvs <NickRibs@hotmail.com>
2023-04-26 18:58:52 -0400

    Created Nav component

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { slide } from "svelte/transition";
+
+    const links = [
+        {text: "Home", url: "/"},
+        {text: "About", url: "/about"},
+    ]
+</script>
+
+<nav transition:slide>
+    {#each links as link}
+        <a href="{link.url}">{link.text}</a>
+    {/each}
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: center;
+    }
+
+    nav a {
+        padding: 1em;
+        color: gray;
+    }
+</style>
