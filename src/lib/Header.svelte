224ff1a0e7831696e1f3fa65a6d363459c921099
ntvs <NickRibs@hotmail.com>
2023-04-26 18:59:38 -0400

    Added menu button to show/hide nav

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,14 +1,58 @@
+<script>
+    import { slide } from "svelte/transition";
+
+    //components
+    import Nav from "./Nav.svelte";
+
+    //state
+    let menuOpen = false;
+</script>
+
 <header>
     <div class="container">
+        <div class="left">
+            <button class="menu" on:click={() => {menuOpen = !menuOpen}}>
+                {#if menuOpen}
+                    <p transition:slide style="transform: rotate(45deg);">+</p>
+                {:else}
+                    <p transition:slide>=</p>
+                {/if}
+            </button>
+        </div>
         <a href="/"><img src="/favicon.png" alt="Logo"></a>
+        <div class="right"></div>
     </div>
 </header>
 
+{#if menuOpen}
+    <Nav />
+{/if}
+
 <style>
-    header {
+    .container {
         display: flex;
         align-items: center;
-        padding: 1em 0;
+        padding: 1em;
+    }
+
+    .left, .right {
+        flex: 1;
+    }
+
+    .menu {
+        border: none;
+        font-size: 2em;
+        background-color: rgba(0, 0, 0, 0);
+        color: lightgray;
+
+        width: 50px;
+        height: 50px;
+
+        transition: var(--transition-slow);
+    }
+
+    .menu:hover, .menu:active {
+        color: gray;
     }
 
     img {
