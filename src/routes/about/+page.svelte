311ce54630b81bcd58c7850ac3c051f26b20944f
ntvs <NickRibs@hotmail.com>
2023-04-26 20:32:59 -0400

    Created about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+
+    //components
+    import SocialLink from "../../lib/SocialLink.svelte"
+
+    //fetched content
+    export let data;
+
+    let profile = {
+        about: {
+            img: "",
+            img_alt: "",
+            desc: ""
+        },
+        links: []
+    };
+    profile = data.profile;
+
+</script>
+
+<svelte:head>
+    <title>About | Nick Ribeiro</title>
+</svelte:head>
+
+<main class="gridContainer">
+    <img src="{profile.about.img}" alt="{profile.about.img_alt}">
+    <p>{profile.about.desc}</p>
+</main>
+
+<div class="links">
+    {#each profile.links as link}
+        <SocialLink options={{icon: link.icon, link: link.link}}/>
+    {/each}
+</div>
+
+<style>
+    main {
+        align-items: center;
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    }
+
+    img, p {
+        width: min(400px, 100%);
+    }
+
+    p {
+        color: gray;
+        line-height: 2em;
+        padding: 1em;
+        justify-self: start;
+    }
+
+    img {
+        justify-self: end;
+    }
+
+    .links {
+        margin-top: 1em;
+        text-align: center;
+    }
+
+    @media screen and (max-width: 615px) {
+        p, img {
+            justify-self: center;
+        }
+    }
+</style>
