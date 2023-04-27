37e8861e96b8741c221b0a9fce13d7d9ebef5d6f
ntvs <NickRibs@hotmail.com>
2023-04-27 11:52:02 -0400

    Added tag component

---
+++ src/lib/Tag.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let object = {
+        text: "",
+        color: "white",
+        bgcolor: "gray"
+    };
+</script>
+
+<p class="tag" style="color: {object.color}; background-color: {object.bgcolor}">{object.text}</p>
+
+<style>
+    .tag {
+        color: white;
+        background-color: #84253b;
+        border-radius: 0.5em;
+        padding: 0.3em;
+        font-size: 0.9em;
+        display: inline-block;
+        margin: 0.1em;
+    }
+</style>
